49a6981afad629fd725e7ef88bd84515636ca1a6
joshuay03 <joshuay03@users.noreply.github.com>
2025-07-18 16:28:22 +0000

    deploy: a76a3efadb753bbe01a400d46a9a6148b35d3754

--- lib/mmap-ruby.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require "mmap-ruby/mmap_ruby"
-require "mmap-ruby/mmap"
-
-Mmap = MmapRuby::Mmap
--- test/test_helper.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "mmap-ruby"
-
-require "minitest/autorun"
